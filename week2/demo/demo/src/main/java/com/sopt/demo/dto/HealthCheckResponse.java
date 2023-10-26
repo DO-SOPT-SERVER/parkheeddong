56c8c6518a49336925f0d09a64aaf5f7e6aa02aa
parkheeddong <phj7214go@gmail.com>
2023-10-26 19:41:48 +0900

    [etc] 불필요 코드 제거

--- week2/demo/demo/src/main/java/com/sopt/demo/dto/HealthCheckResponse.java
+++
@@ -1,9 +0,0 @@
-package com.sopt.demo.dto;
-
-public class HealthCheckResponse {
-    private static final String STATUS = "OK";
-    private String status;
-    public HealthCheckResponse() {
-        this.status = STATUS;
-    }
-}
