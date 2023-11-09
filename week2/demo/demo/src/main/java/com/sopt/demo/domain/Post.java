4c2561b5694f650e5a3edea6fa35adc82ef677e5
parkheeddong <phj7214go@gmail.com>
2023-11-09 17:43:22 +0900

    [feat] PostJpaRepository 생성

--- week2/demo/demo/src/main/java/com/sopt/demo/domain/Post.java
+++ week2/demo/demo/src/main/java/com/sopt/demo/domain/Post.java
@@ -1,5 +1,6 @@
 package com.sopt.demo.domain;
 
+import com.fasterxml.jackson.databind.ser.Serializers.Base;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -18,7 +19,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Table(name = "post")
-public class Post {
+public class Post extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
