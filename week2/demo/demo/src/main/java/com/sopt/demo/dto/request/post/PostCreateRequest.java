8064d7edaf8bedc95c539a709bf0a9b99dd0a224
parkheeddong <phj7214go@gmail.com>
2023-11-09 18:13:21 +0900

    [feat] 생성 api 를 위한 PostController, PostSerivce 생성

---
+++ week2/demo/demo/src/main/java/com/sopt/demo/dto/request/post/PostCreateRequest.java
@@ -0,0 +1,4 @@
+package com.sopt.demo.dto.request.post;
+
+public record PostCreateRequest(String title, String content) {
+}
---
+++ week2/demo/demo/src/main/java/com/sopt/demo/service/PostService.java
@@ -0,0 +1,42 @@
+package com.sopt.demo.service;
+
+import com.sopt.demo.domain.Member;
+import com.sopt.demo.domain.Post;
+import com.sopt.demo.dto.request.post.PostCreateRequest;
+import com.sopt.demo.repository.MemberJpaRepository;
+import com.sopt.demo.repository.PostJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class PostService {
+
+    private final PostJpaRepository postJpaRepository;
+    private final MemberJpaRepository memberJpaRepository;
+
+    @Transactional
+    public String create(PostCreateRequest request, Long memberId) {
+        Member member = memberJpaRepository.findByIdOrThrow(memberId);
+        Post post = postJpaRepository.save(
+                Post.builder()
+                        .member(member)
+                        .title(request.title())
+                        .content(request.content()).build());
+        return post.getPostId().toString();
+    }
+
+    /*
+    [1] memberJpaRepository.findByIdOrThrow(memberId);
+    -> 게시물 작성자 ID를 이용해서 해당 멤버를 조회하고, 없을 경우엔 예외를 던진다
+    [2] Post.builder().member(member).title(request.title()).content(request.content()).build())
+    -> 빌더 디자인 패턴을 이용하여 Post 엔티티 객체를 생성한다
+    [3] Post post = postJpaRepository.save
+    -> 생성된 Post 엔티티를 Jpa Repository에 저장한다
+    [4] return post.getPostId().toString();
+    -> 생성된 Post의 Id를 문자열로 반환한다
+     */
+}
+
